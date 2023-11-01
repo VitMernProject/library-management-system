c92cd3b9e8e126505f5d624fb9665726d1670ad7
sowjanya1308 <sowjanyasankara@gmail.com>
2023-11-01 22:27:50 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,19 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import SignInPage from "./screens/signin/Signin";
 import registerPage from "./screens/register/registerpage";
 import loginPage from "./screens/login/loginpage";
+import StartPage from "./screens/startpage/start";
 
 const App = () => {
   return (
     <>
     {/* <AppBar/> */}
       <Routes>
-        <Route path="/" Component={Home} />
+        <Route path="/" Component={StartPage} />
         <Route path="*" element={<Navigate replace to={"/"} />} />
         <Route path="/signin" Component={SignInPage}/>
         <Route path="/register" Component={registerPage}/>
         <Route path="/login" Component={loginPage}/>
+        <Route path="/home" Component={Home}/>
       </Routes>
     </>
   );
