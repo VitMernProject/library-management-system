74bfb2c43403af4ca64262860ab9d7c558a6d0fe
Geswar003 <geswar003@gmail.com>
2023-11-13 21:12:24 +0530

    icons

--- client/src/screens/components/sidenavbar.js
+++ client/src/screens/components/sidenavbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './nav.css';
-import {AiOutlineHome,AiOutlineBook} from 'react-icons/ai';
+import {AiOutlineHome,AiOutlineBook,AiOutlineIssuesClose} from 'react-icons/ai';
 import {BiMessageDetail} from 'react-icons/bi';
 import {PiBooksLight} from 'react-icons/pi';
-import {LiaBookSolid} from 'react-icons/lia';
+import {LiaBookSolid,LiaSwatchbookSolid} from 'react-icons/lia';
 import {HiOutlineLogout} from 'react-icons/hi';
 import { NavLink } from 'react-router-dom';
+import {IoIosPeople} from 'react-icons/io'
 
 const SideNavBar = () => {
     const role = localStorage.getItem('role');
@@ -18,9 +19,9 @@ const SideNavBar = () => {
                   {(role === "student")?<li><NavLink className="a" to="/recommended"><LiaBookSolid className='me-3' />Recommended Books</NavLink></li>:<></>}
                   {(role === "student")?<li><NavLink className="a" to="/currentlyissued"><AiOutlineBook className='me-3' />Currently Issued Books</NavLink></li>:<></>}
                   {(role === "admin")?<li><NavLink className="a" to="/addBook"><AiOutlineBook className='me-3' />Add Book</NavLink></li>:<></>}
-                  {(role === "admin")?<li><NavLink className="a" to="/manageStudents"><HiOutlineLogout className='me-3' />Manage Students</NavLink></li>:<></>}
-                  {(role === "admin")?<li><NavLink className="a" to="/issueRequest"><HiOutlineLogout className='me-3' />Issue Request</NavLink></li>:<></>}
-                  {(role === "admin")?<li><NavLink className="a" to="/allissued"><HiOutlineLogout className='me-3' />All issued Book</NavLink></li>:<></>}
+                  {(role === "admin")?<li><NavLink className="a" to="/manageStudents"><IoIosPeople className='me-3' />Manage Students</NavLink></li>:<></>}
+                  {(role === "admin")?<li><NavLink className="a" to="/issueRequest"><AiOutlineIssuesClose className='me-3' />Issue Request</NavLink></li>:<></>}
+                  {(role === "admin")?<li><NavLink className="a" to="/allissued"><LiaSwatchbookSolid className='me-3' />All issued Book</NavLink></li>:<></>}
                   {/* <li><NavLink className="a" to="/home"><HiOutlineLogout className='me-3' />Logout</NavLink></li> */}
               </ul>
           </div>
